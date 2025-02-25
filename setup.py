4c69d18747c8cca3e6fb5bdff937499aa9252fa6
echo <licheng_xu@zju.edu.cn>
2025-02-25 13:12:08 +0800

    fix numpy version mismatch warning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
         return [line.strip() for line in f if line.strip() and not line.startswith('#')]
 setup(
     name="rxngraphormer",
-    version="0.6.1",
+    version="0.6.2",
     description="Package for a novel graph-based transformer model for reaction prediction",
     keywords=[],
     url="https://github.com/licheng-xu-echo/RXNGraphormer",
